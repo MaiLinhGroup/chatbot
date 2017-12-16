4c22008dc5cd96eb566531d0e3ff9a5c6c8d5758
mle <mle@MLEDith.fritz.box>
2017-12-16 14:58:33 +0100

    add test

--- chatbot_test.go
+++ chatbot_test.go
@@ -26,3 +26,21 @@ func TestReadChatBotCfg(t *testing.T) {
 	assert.NotContains(t, cfg.configs, "Hase")
 	assert.Equal(t, iBot, cfg.configs["DRVBot"])
 }
+
+func TestGetNewChatBot(t *testing.T) {
+	//Given
+	iBot := BaseBot{
+		FirstName: "Walter",
+		LastName:  "Riester",
+		UserName:  "RWalterBot",
+		ID:        490569313,
+		Token:     "490569313:AAEp10AaG9omULlCLEmA_Lp8QYhVchGvtgQ",
+	}
+
+	//When
+	bot, err := GetNewChatBot(iBot)
+
+	//Then
+	assert.NoError(t, err)
+	assert.Equal(t, iBot.ID, bot.Self.ID)
+}
