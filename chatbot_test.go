f32bb0a71040756d63122523ba2939ac2c4dded1
mle <mle@MLEDith.fritz.box>
2018-01-01 22:36:27 +0100

    comment out tf

--- chatbot_test.go
+++ chatbot_test.go
@@ -46,16 +46,16 @@ func TestGetNewChatBot(t *testing.T) {
 }
 
 func TestTravelTime(t *testing.T) {
-	testCases := []struct {
-		desc string
-	}{
-		{
-			desc: "",
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
+	// testCases := []struct {
+	// 	desc string
+	// }{
+	// 	{
+	// 		desc: "",
+	// 	},
+	// }
+	// for _, tC := range testCases {
+	// 	t.Run(tC.desc, func(t *testing.T) {
 
-		})
-	}
+	// 	})
+	// }
 }
