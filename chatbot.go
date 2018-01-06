810a317b39fc39e07c907713f8ed483960847bff
mle <mle@MLEDith.fritz.box>
2018-01-06 22:07:59 +0100

    add new bot token for chatlottebot

--- chatbot.go
+++ chatbot.go
@@ -1,5 +1,41 @@
 package main
 
-const (
-	botAPIToken = "490569313:AAEp10AaG9omULlCLEmA_Lp8QYhVchGvtgQ"
+import (
+	"log"
+
+	"gopkg.in/telegram-bot-api.v4"
 )
+
+const (
+	//@ChatLotteBot
+	botAPIToken = "503887514:AAHOnl7OiyDk6oBPvHuJBEadlBOxFTnGxlk"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(botAPIToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
