ba3f810b3bd6374a7c02d7d3b54de67059488f99
ml <ml@mac.os>
2018-06-06 00:10:26 +0200

    implementing example code from tgbot api

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
@@ -16,13 +19,53 @@ func main() {
 	// retrieve the token pass by env var to pass it to chatbot
 	tkn := os.Getenv("TOKEN")
 	if tkn == "" {
-		// if no token is passing
-		// then the user may just forget it,
-		// but we know how to handle this error
-		// (inform the user about our need)
-		// that's why we won't panic here
-		log.Fatal("Please passing the value for the bot api token to continue using the chatbot.")
+		log.Println("Please passing a valid token.We want something like 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11.")
 	}
 
 	// now we can call the chatbot with the token
+	bot, err := tgbot.NewBotAPI(tkn)
+	if err != nil {
+		log.Panic("in tgbot NewBotAPI:", err)
+	}
+
+	bot.Debug = true
+
+	ucfg := tgbot.NewUpdate(0)
+	ucfg.Timeout = 60
+
+	udp, err := bot.GetUpdatesChan(ucfg)
+	if err != nil {
+		log.Panic("in tgbot GetUpdatesChan:", err)
+	}
+	time.Sleep(time.Millisecond * 500)
+	udp.Clear()
+
+	// ch := make(chan string)
+
+	// go func() {
+	// defer close(ch)
+
+	for u := range udp {
+		if u.Message == nil {
+			continue
+		}
+
+		log.Printf("From [%s] : %s\n", u.Message.From.UserName, u.Message.Text)
+		msg := tgbot.NewMessage(u.Message.Chat.ID, u.Message.Text)
+		msg.ReplyToMessageID = u.Message.MessageID
+
+		reply, err := bot.Send(msg)
+		if err != nil {
+			log.Panic("in tgbot Send", err)
+		}
+
+		log.Printf("Reply with ID %v and %s.\n", msg.ReplyToMessageID, reply.Text)
+
+		// ch <- u.Message.Text
+	}
+	// }()
+
+	// for msg := range ch {
+	// 	// use your string :)
+	// }
 }
