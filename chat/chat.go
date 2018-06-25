d8a762b280e862c4f9612ff71e87e8dc1ad80fb2
ml <ml@mac.os>
2018-06-25 21:35:12 +0200

    update chat module description and comments

--- chat/chat.go
+++ chat/chat.go
@@ -1,5 +1,9 @@
 package chat
 
+// The chat module interacts with users via the Telegram Bot API.
+// It processes user requests and makes them accessible for other
+// modules. It also sends back the results of other modules to the users.
+
 import (
 	"errors"
 	"os"
@@ -12,19 +16,21 @@ import (
 // monkey patching :monkey_face:
 var osGetEnv = os.Getenv
 
-// Bot ...
+// Bot contains the dependencies to leverage the Telegram Bot API
 type Bot struct {
 	API          *tgbot.BotAPI      // interaction with Telegram Bot API
 	UpdateConfig tgbot.UpdateConfig // contains information about update request
 }
 
-// Message ...
+// Message is constructed of a unique ID which is used to identify the user
+// who has sent the request and the request itself
 type Message struct {
 	ID   int64
 	Text string
 }
 
-// New ...
+// New authentificates a new Bot struct with the provided token at
+// the Telegram Bot API and returns it ready-to-use to interact with the API
 func New() (*Bot, error) {
 	// Each bot is given a unique authentication token when it is created
 	token := osGetEnv("TOKEN")
@@ -39,15 +45,18 @@ func New() (*Bot, error) {
 	}
 
 	// uncomment this for tgbot debug
-	// botAPI.Debug = true
+	botAPI.Debug = true
 
+	// contains config information about updating user requests
 	updateCfg := tgbot.NewUpdate(0)
 	updateCfg.Timeout = 60
 
 	return &Bot{API: botAPI, UpdateConfig: updateCfg}, nil
 }
 
-// Chat ...
+// Chat either receiving user requests via the update channel or
+// sending results back to user using the send functionality of
+// the Telegram Bot API
 func (bot *Bot) Chat(msgCh chan Message) error {
 	// Get update channel to receive messages from user
 	updCh, err := bot.API.GetUpdatesChan(bot.UpdateConfig)
